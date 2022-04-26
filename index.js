a60fad658eec4b04a7acbbc343f0495fe89fba63
MD Rafi Ahmed <76904693+rafiahmed47@users.noreply.github.com>
2022-04-27 05:34:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;;
 const { MongoClient } = require('mongodb');
 const fileUpload = require('express-fileupload')
 require('dotenv').config()
